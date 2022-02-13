6775f4c91be33802a365de1d3ce5fc766c70304b
Valery Pobelenskiy <valery_po85@mail.ru>
2022-02-13 23:14:51 +0600

    relase createX

---
+++ src/js/components/components.js
@@ -0,0 +1,11 @@
+const selects = document.querySelectorAll(".form-field__select");
+selects.forEach(el => {
+    new Choices(el, {
+     shouldSort: false,
+     position: "bottom",
+     searchEnabled: false
+
+    })
+})
+
+
